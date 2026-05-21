214bc24bf547d79a87219e3b2f3a5da902e3bc64
Salah Saleh <salahsaleh1015@gmail.com>
2026-05-21 18:22:16 +0300

    add checkout repo impl

--- lib/features/checkout/data/repos/checkout_repo.dart
+++ lib/features/checkout/data/repos/checkout_repo.dart
@@ -1,9 +1,26 @@
 import 'package:dartz/dartz.dart';
 import 'package:gateway_payments_app/core/errors/failures.dart';
+import 'package:gateway_payments_app/core/utils/stripe_service.dart';
 import 'package:gateway_payments_app/features/checkout/data/models/payment_intent_input_model.dart';
 
 abstract class CheckoutRepo {
   Future<Either<Failure, void>> makePayment({
     required PaymentIntentInputModel paymentIntentInput,
   });
+}
+
+class CheckoutRepoImpl implements CheckoutRepo {
+  StripeService stripeService = StripeService();
+
+  @override
+  Future<Either<Failure, void>> makePayment({
+    required PaymentIntentInputModel paymentIntentInput,
+  }) async {
+    try {
+      await stripeService.makePayment(paymentIntentInput);
+      return Right(null);
+    } catch (e) {
+      return Left(ServerFailure(errMessage: e.toString()));
+    }
+  }
 }
