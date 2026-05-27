70c5dfa345f671055b424931d44b62ed55a2f196
Salah Saleh <salahsaleh1015@gmail.com>
2026-05-27 18:01:37 +0300

    create  ephemeral key request

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:gateway_payments_app/core/utils/api_service.dart';
 import 'package:gateway_payments_app/core/utils/app_keys.dart';
+import 'package:gateway_payments_app/features/checkout/data/models/ephemeral_key_model/ephemeral_key_model.dart';
 import 'package:gateway_payments_app/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:gateway_payments_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
@@ -44,5 +45,22 @@ class StripeService {
     );
 
     await displayPaymentSheet();
+  }
+
+  Future<EphemeralKeyModel> createEphemeralKey(
+      {required String customerId}) async {
+    var response = await apiService.post(
+        body: {'customer': customerId},
+        contentType: Headers.formUrlEncodedContentType,
+        url: 'https://api.stripe.com/v1/ephemeral_keys',
+        token: AppKeys.secretKey,
+        headers: {
+          'Authorization': "Bearer ${AppKeys.secretKey}",
+          'Stripe-Version': '2023-08-16',
+        });
+
+    var ephermeralKey = EphemeralKeyModel.fromJson(response.data);
+
+    return ephermeralKey;
   }
 }
