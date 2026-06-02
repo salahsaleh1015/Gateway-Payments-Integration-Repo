318d47744b62f7ae36ea76813d646c0e84ae9858
Salah Saleh <salahsaleh1015@gmail.com>
2026-06-02 23:19:06 +0300

    add paypal models

---
+++ lib/features/checkout/data/models/paypal_models/item_list_model/item_list_model.dart
@@ -0,0 +1,17 @@
+import 'package:gateway_payments_app/features/checkout/data/models/paypal_models/item_list_model/item.dart';
+
+class ItemListModel {
+  List<OrderItemModel>? orders;
+
+  ItemListModel({this.orders});
+
+  factory ItemListModel.fromJson(Map<String, dynamic> json) => ItemListModel(
+    orders: (json['items'] as List<dynamic>?)
+        ?.map((e) => OrderItemModel.fromJson(e as Map<String, dynamic>))
+        .toList(),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'items': orders?.map((e) => e.toJson()).toList(),
+  };
+}
